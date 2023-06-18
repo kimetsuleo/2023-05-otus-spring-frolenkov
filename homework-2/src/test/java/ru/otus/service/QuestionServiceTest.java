51d06fd753739214e3d86e0d62767532f9ca34fc
kimetsuleo <leo.frolenkov@gmail.com>
2023-06-18 13:46:01 +0300

    create test package

---
+++ homework-2/src/test/java/ru/otus/service/QuestionServiceTest.java
@@ -0,0 +1,19 @@
+package ru.otus.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author kimetsu - 18.06.2023 - 13:41
+ */
+@DisplayName("class which test QuestionService")
+public class QuestionServiceTest {
+
+
+
+    @Test
+    void getAllTest() {
+
+    }
+
+}
