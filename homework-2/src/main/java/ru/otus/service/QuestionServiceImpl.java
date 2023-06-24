957fd46cf4f66f4955677eeb2a50be042a799933
kimetsuleo <leo.frolenkov@gmail.com>
2023-06-24 17:23:10 +0300

    refactor code

--- homework-2/src/main/java/ru/otus/service/QuestionServiceImpl.java
+++ homework-2/src/main/java/ru/otus/service/QuestionServiceImpl.java
@@ -21,50 +21,18 @@ public class QuestionServiceImpl implements QuestionService {
 
     private final IOService ioService;
 
-    private List<List<Answer>> listAnswer = List.of(
-            List.of(
-                    new Answer("100"),
-                    new Answer("116"),
-                    new Answer("125")
-            ),
-            List.of(
-                    new Answer("6"),
-                    new Answer("4"),
-                    new Answer("2")
-            ),
-            List.of(
-                    new Answer("Russia"),
-                    new Answer("USA"),
-                    new Answer("China")
-            ),
-            List.of(
-                    new Answer("Great Britain"),
-                    new Answer("Mexico"),
-                    new Answer("Japan")
-            ),
-            List.of(
-                    new Answer("maybe"),
-                    new Answer("no"),
-                    new Answer("yes")
-            ));
-
-
     @Override
     public void startTesting() {
         int count = 0;
 
         List<Question> questions = this.questionDao.getAllQuestions();
 
-        for (int i = 0; i < questions.size(); i++) {
-            questions.get(i).setOptionAnswers(listAnswer.get(i));
-        }
-
         User user = this.userDao.registerUser();
 
         for (Question question : questions) {
             this.ioService.print(question.getTitle() + " ");
             List<Answer> listAnswer = question.getOptionAnswers();
-            this.ioService.println(listAnswer.stream().map(Answer::getText).collect(Collectors.joining(",")));
+            this.ioService.println(listAnswer.toString());
             String s = this.giveAnAnswer();
             if (question.getAnswer().equalsIgnoreCase(s)) {
                 count++;
