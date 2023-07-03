fe9e6df65e86661a842315bc794f0f04f5ac166d
kimetsuleo <leo.frolenkov@gmail.com>
2023-07-03 22:34:11 +0300

    refactor sql query

--- homework-5/src/main/resources/init.sql
+++ homework-5/src/main/resources/init.sql
@@ -1,12 +1,12 @@
 create table if not exists genres
 (
-    id    integer primary key,
+    id    identity primary key,
     title character varying
 );
 
 create table if not exists authors
 (
-    id        integer primary key,
+    id        identity primary key,
     firstName character varying not null,
     lastName  character varying not null,
     dof       date              not null
@@ -14,7 +14,7 @@ create table if not exists authors
 
 create table if not exists books
 (
-    id             integer primary key,
+    id             identity primary key,
     title          character varying not null,
     author_id      integer           not null,
     genre_id       integer           not null,
--- homework-5/src/main/resources/insert_data.sql
+++ homework-5/src/main/resources/insert_data.sql
@@ -1,26 +1,26 @@
-insert into genres(id, title)
-values (1, 'Triller'),
-       (2, 'Roman'),
-       (3, 'Comedy'),
-       (4, 'Horror'),
-       (5, 'Manga'),
-       (6, 'IT'),
-       (7, 'Fantasy');
+insert into genres(title)
+values ('Triller'),
+       ('Roman'),
+       ('Comedy'),
+       ('Horror'),
+       ('Manga'),
+       ('IT'),
+       ('Fantasy');
 
-insert into authors(id, firstName, lastName, dof)
-values (1, 'Jack', 'London', '1876-01-12'),
-       (2, 'Robert', 'Martin', '1952-12-05'),
-       (3, 'Anjey', 'Sapkowski', '1948-06-12'),
-       (4, 'Oscar', 'Wilde', '1854-10-16'),
-       (5, 'Erich', 'Remarque', '1898-06-22'),
-       (6, 'Koyoharu', 'Gotouge', '1989-05-05');
+insert into authors(firstName, lastName, dof)
+values ('Jack', 'London', '1876-01-12'),
+       ('Robert', 'Martin', '1952-12-05'),
+       ('Anjey', 'Sapkowski', '1948-06-12'),
+       ('Oscar', 'Wilde', '1854-10-16'),
+       ('Erich', 'Remarque', '1898-06-22'),
+       ('Koyoharu', 'Gotouge', '1989-05-05');
 
-insert into books(id, title, author_id, genre_id, publication_at)
-values (1, 'The Witcher', 3, 7, '2021-01-01'),
-       (2, 'Martin Eden', 1, 2, '1952-01-01'),
-       (3, 'Clean Code', 2, 6, '1948-01-01'),
-       (4, 'Kimetsu no Yaiba', 6, 7, '1854-01-01'),
-       (5, 'The picture of Dorian Grey', 4, 7, '1898-01-01'),
-       (6, 'No changes on the Western Front', 5, 2, '1989-01-01');
+insert into books(title, author_id, genre_id, publication_at)
+values ('The Witcher', 3, 7, '2021-01-01'),
+       ('Martin Eden', 1, 2, '1952-01-01'),
+       ('Clean Code', 2, 6, '1948-01-01'),
+       ('Kimetsu no Yaiba', 6, 7, '1854-01-01'),
+       ('The picture of Dorian Grey', 4, 7, '1898-01-01'),
+       ('No changes on the Western Front', 5, 2, '1989-01-01');
 
 
